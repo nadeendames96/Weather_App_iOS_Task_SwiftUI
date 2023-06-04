e89619d21db1255c097ae3cbad1ce00ef00ea3e9
Nadeen Omar Dames <nana1196-4-20@hotmail.com>
2023-06-04 19:39:53 +0300

    New Weather Aplication Jun 4 2023

---
+++ Weather_App_Task/Weather_App_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_TaskApp.swift
+//  Weather_App_Task
+//
+//  Created by Nadeen Dames on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather_App_TaskUITests/Weather_App_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Weather_App_TaskUITestsLaunchTests.swift
+//  Weather_App_TaskUITests
+//
+//  Created by Nadeen Dames on 04/06/2023.
+//
+
+import XCTest
+
+final class Weather_App_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
